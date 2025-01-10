6fffb3c32babc6e3f3a39728289cebdd3c721fb3
opotapov <oleksii.potapov@dataart.com>
2025-01-10 16:38:28 +0200

    Change redirectUri to victorious-coast

--- src/authConfig.js
+++ src/authConfig.js
@@ -4,7 +4,7 @@ export const msalConfig = {
     clientId: "95b9529c-a14b-404d-a917-66c4ffa83e0f", // Replace with your Application (client) ID
     authority:
       "https://anthonynolandonordev.b2clogin.com/anthonynolandonordev.onmicrosoft.com/B2C_1_sign_up_sign_in", // Replace <YOUR_TENANT_NAME>
-    redirectUri: "https://red-river-08870a603.4.azurestaticapps.net/", // Must match the Redirect URI in Azure portal
+    redirectUri: "https://victorious-coast-04905fd03.4.azurestaticapps.net", // Must match the Redirect URI in Azure portal
     knownAuthorities: ["anthonynolandonordev.b2clogin.com"], // Replace <YOUR_TENANT_NAME>
   },
   cache: {
